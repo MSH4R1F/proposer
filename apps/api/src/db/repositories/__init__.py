ad26123a5959b598a5d40ab651cd9f02d9cd785c
Mohamed Sharif <msharabdq@gmail.com>
2026-04-29 20:44:32 +0100

    feat(db): re-export all repositories from db.repositories

---
+++ apps/api/src/db/repositories/__init__.py
@@ -0,0 +1,15 @@
+from apps.api.src.db.repositories.disputes_repo import DisputesRepo
+from apps.api.src.db.repositories.evidence_repo import EvidenceRepo
+from apps.api.src.db.repositories.kg_repo import KnowledgeGraphRepo
+from apps.api.src.db.repositories.mediations_repo import MediationsRepo
+from apps.api.src.db.repositories.predictions_repo import PredictionsRepo
+from apps.api.src.db.repositories.sessions_repo import SessionsRepo
+
+__all__ = [
+    "DisputesRepo",
+    "EvidenceRepo",
+    "KnowledgeGraphRepo",
+    "MediationsRepo",
+    "PredictionsRepo",
+    "SessionsRepo",
+]
